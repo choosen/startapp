4190b0a1132e4b4e547571be9093094ecef6b04d
ChooseN <chooosen@gmail.com>
2016-07-24 00:19:38 +0200

    end of 7th chapter

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -39,7 +39,7 @@ class ProductTest < ActiveSupport::TestCase
       title:        'My Book 2 from product with url passed',
       description:  'descriptionnnn',
       price:        1,
-      image_url:    iamge_url
+      image_url:    image_url
     )
   end
   
@@ -52,6 +52,19 @@ class ProductTest < ActiveSupport::TestCase
     
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} should be invalid"
-    end
+    end    
+  end
+  
+  test 'product is not valid without a unique title' do
+    product = Product.new(
+      title:        products(:ruby).title,
+      description:  'yyyy',
+      price:        1,
+      image_url:    'fred.gif'
+    )
+    assert !product.save
+    assert_equal 'has already been taken', product.errors[:title].join('; ')
+    #assert_equal I18n.translate('activerecord.errors.messages.taken'),
+    #             product.errors[:title].join('; ')
   end
 end
