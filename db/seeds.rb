a71ffa6c0b7b0a9d849cf04cd785cd15540928fa
ChooseN <chooosen@gmail.com>
2016-07-23 08:24:01 +0200

    after rubocop od schema.. na starcie nie ma co sie tym przejmować, bo wygenerowany kod nie jest compatible

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside
+# the db with db:setup).
 #
 # Examples:
 #
@@ -12,29 +14,37 @@
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book
+# information.
 #---
 # encoding: utf-8
+# notation by rubocop
 Product.delete_all
-Product.create(title: 'CoffeeScript',
-  description: 
-    %{<p>
+Product.create(
+  title: 'CoffeeScript',
+  description:
+
+    %(<p>
         CoffeeScript is JavaScript done right. It provides all of JavaScript's
 	functionality wrapped in a cleaner, more succinct syntax. In the first
 	book on this exciting new language, CoffeeScript guru Trevor Burnham
 	shows you how to hold onto all the power and flexibility of JavaScript
 	while writing clearer, cleaner, and safer code.
-      </p>},
-  image_url:   'cs.jpg',    
-  price: 36.00)
-# . . .
+      </p>),
+  image_url:
+
+    'cs.jpg',
+  price:
+
+    36.00
+)
+# original from book notation
 Product.create(title: 'Programming Ruby 1.9',
-  description:
-    %{<p>
+  description:     %(<p>
         Ruby is the fastest growing and most exciting dynamic language
         out there. If you need to get working programs delivered fast,
         you should add Ruby to your toolbox.
-      </p>},
+      </p>),
   image_url: 'ruby.jpg',
   price: 49.95)
 # . . .
@@ -49,5 +59,6 @@ Product.create(title: 'Rails Test Prescriptions',
         procedures for Rails 2 and Rails 3, and introduces popular add-ons,
         including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
       </p>},
-  image_url: 'rtp.jpg',
-  price: 34.95)
+  image_url:    'rtp.jpg',
+  price:        34.95
+)
