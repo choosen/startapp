d109348f7fc749ff74990ce979f4cf9b22f27dd1
ChooseN <chooosen@gmail.com>
2016-07-24 00:37:30 +0200

    rubocopped product_test.rb

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,10 +1,12 @@
 require 'test_helper'
 
+# Przed klasa warto zawsze opisac zeby rubocop sie nieplul a inni wiedzieli co
+# sie dzieje w kodzie
 class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  
+
   test 'product attributes must not be empty' do
     product = Product.new
     assert product.invalid?
@@ -13,27 +15,35 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
-  
+
   test 'ogranicz czas testowania/pracy do rozsądnych godzin pracy' do
-    assert (Time.now.hour > 6 && Time.now.hour < 18) ||
-      (Time.now.hour > 21 && Time.now.hour <= 24)
+    assert (
+             (
+               Time.now.hour > 6 &&
+               Time.now.hour < 18
+             ) ||
+             (
+               Time.now.hour > 21 &&
+               Time.now.hour <= 24
+             )
+           )
   end
-  
+
   test 'product price must be positive' do
     product = Product.new(
-                      title:        'My Book Title',
-                      description:  'yyy',
-                      image_url:    'zzz.jpg'
+      title:        'My Book Title',
+      description:  'yyy',
+      image_url:    'zzz.jpg'
     )
     product.price = -1
     assert product.invalid?
     assert_equal 'must be greater than or equal to 0.01',
-      product.errors[:price].join('; ')
-    
+                 product.errors[:price].join('; ')
+
     product.price = 1
     assert product.valid?
   end
-  
+
   def new_product(image_url)
     Product.new(
       title:        'My Book 2 from product with url passed',
@@ -42,19 +52,19 @@ class ProductTest < ActiveSupport::TestCase
       image_url:    image_url
     )
   end
-  
+
   test 'image url' do
-    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.c/xfred.png }
-    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok = %w(fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.c/xfred.png)
+    bad = %w(fred.doc fred.gif/more fred.gif.more)
     ok.each do |name|
       assert new_product(name).valid?, "#{name} should be valid"
     end
-    
+
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} should be invalid"
-    end    
+    end
   end
-  
+
   test 'product is not valid without a unique title' do
     product = Product.new(
       title:        products(:ruby).title,
@@ -64,7 +74,7 @@ class ProductTest < ActiveSupport::TestCase
     )
     assert !product.save
     assert_equal 'has already been taken', product.errors[:title].join('; ')
-    #assert_equal I18n.translate('activerecord.errors.messages.taken'),
+    # assert_equal I18n.translate('activerecord.errors.messages.taken'),
     #             product.errors[:title].join('; ')
   end
 end
