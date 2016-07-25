36a8f7ce559046c6208aa5d00162b281b87c382b
ChooseN <chooosen@gmail.com>
2016-07-26 00:26:30 +0200

    cos jeszcze nie dziala trzemanie w sesji cart_id..

---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+# no wez cos opisz
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    # assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select 'h3', 'Programming Ruby 2.3.1'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+  end
+
+end
